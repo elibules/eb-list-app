09e3a26f6ac9bd9f4d2b12b5561482d7aa46121f
elibules <elibules@gmail.com>
2021-10-06 16:55:02 -0400

    pwa update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,16 @@ module.exports = {
     siteUrl: "https://in-info-web4.informatics.iupui.edu/~ebules/n322/list-app",
     title: "List App",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        short_name: "Todo List",
+        display: "standalone",
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-offline",
+  ],
   pathPrefix: "/~ebules/n322/list-app",
 };
